de4a511a36e35bb89d3da89e13ea3e64f9d08c4a
Matheus Faé <matheusfae@gmail.com>
2026-07-03 12:36:26 -0300

    Endpoints iniciais finalizados

--- APICompanion/Controllers/UsuarioController.cs
+++ APICompanion/Controllers/UsuarioController.cs
@@ -14,19 +14,48 @@ namespace APICompanion.Controllers
             _servicoUsuario = servicoUsuario;
         }
 
-        [HttpPost]
-        public IActionResult Cadastro(UsuarioDTOCriacao usuarioDTO)
+        [HttpPost("cadastro")]
+        public async Task<IActionResult> Cadastro(UsuarioDTOCriacao usuarioDTO)
         {
-            _servicoUsuario.Cadastro(usuarioDTO);
+            await _servicoUsuario.Cadastro(usuarioDTO);
             return Ok();
         }
 
-        [HttpGet]
+        [HttpGet("obter")]
         public async Task<IActionResult> ObterPorId(int usuarioId)
         {
             var usuario = await _servicoUsuario.ObterPorId(usuarioId);
 
             return Json(usuario);
-        } 
+        }
+
+        [HttpGet("listar")]
+        public async Task<IActionResult> Listar()
+        {
+            var usuarios = await _servicoUsuario.Listar();
+
+            return Json(usuarios);
+        }
+
+        [HttpPut("atualizar-usuario")]
+        public async Task<IActionResult> AtualizarUsuario(UsuarioDTOAtualizacao dto)
+        {
+            await _servicoUsuario.Atualizacao(dto);
+            return Ok();
+        }
+
+        [HttpPut("atualizar-senha")]
+        public async Task<IActionResult> AtualizarSenha(UsuarioDTOAtualizacaoDeSenha dto)
+        {
+            await _servicoUsuario.AtualizarSenha(dto);
+            return Ok();
+        }
+
+        [HttpDelete("deletar")]
+        public async Task<IActionResult> DeletarUsuario(int usuarioId)
+        {
+            await _servicoUsuario.DeletarSemExcluir(new UsuarioDTODelecao { Id = usuarioId });
+            return Ok();
+        }
     }
 }
